ec9023a274e51ccd2fb88beef0bf2411325ed8c1
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2023-07-17 13:51:19 +0000

    system selectors separetely

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,23 @@
 import { Box, Checkbox, Container, FormControl, Grid, InputLabel, List, ListItem, ListItemText, MenuItem, OutlinedInput, Select, SelectChangeEvent, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { IFormData, IFormProps } from "../App";
+import SystemSelector from "./SystemSelector";
 
 const industries = ['Produkcja', 'Handel', 'Dostawca usług logistycznych', 'Branża farmaceutyczna', 'Branża napojowa', 'Branża odzieżowa', 'Branża chemiczna', 'Przemysł spożywczy', 'Automotive', 'Inna']
 
-const urlASRS =
-  'https://www.jungheinrich.cn/resource/image/540796/landscape_ratio16x10/750/469/4de74d221121a65bc7e0c08b6d4285da/1FA625729DA78A1AD6585E27F629F67B/stage-automatic-small-parts-storage.jpg';
-const urlLRKPRK =
-  'https://www.jungheinrich.cn/resource/image/540918/landscape_ratio16x10/750/469/40bd0c110ea57c96e6b5a51e6d6728ed/1F26C0C524BFC81ADDA1253482D95F1A/stage-small-parts-storage-dynamic.jpg';
-const urlAGV =
-  'https://www.jungheinrich.cn/resource/image/540798/landscape_ratio16x10/750/469/5207e5fdaf2ac8a1858bcdae07a44ab2/FE5BFAE61CB18BBD620473590B9B437E/stage-agv-system.jpg';
-const urlAutoVNA =
-  'https://www.jungheinrich.cn/resource/image/541672/landscape_ratio16x10/750/469/c7d514ecb2cce052105a89c86396a92b/2F274C04399B4D9A89056F7A564042BA/stage-automated-high-rack-stacker.jpg';
+export interface ISystems {
+  [key: string]: {
+    url: string;
+    alt: string;
+  };
+}
+
+const systems: ISystems = {
+  ASRS: {url: 'https://www.jungheinrich.cn/resource/image/540796/landscape_ratio16x10/750/469/4de74d221121a65bc7e0c08b6d4285da/1FA625729DA78A1AD6585E27F629F67B/stage-automatic-small-parts-storage.jpg', alt: 'ASRS'},
+  LRKPRK: {url: 'https://www.jungheinrich.cn/resource/image/540918/landscape_ratio16x10/750/469/40bd0c110ea57c96e6b5a51e6d6728ed/1F26C0C524BFC81ADDA1253482D95F1A/stage-small-parts-storage-dynamic.jpg', alt: 'LRK&PRK'},
+  AGV: {url: 'https://www.jungheinrich.cn/resource/image/540798/landscape_ratio16x10/750/469/5207e5fdaf2ac8a1858bcdae07a44ab2/FE5BFAE61CB18BBD620473590B9B437E/stage-agv-system.jpg', alt: 'AGV'},
+  AutoVNA: {url: 'https://www.jungheinrich.cn/resource/image/541672/landscape_ratio16x10/750/469/c7d514ecb2cce052105a89c86396a92b/2F274C04399B4D9A89056F7A564042BA/stage-automated-high-rack-stacker.jpg', alt: 'AutoVNA'}
+}
 
 //props for the insdustries select
 const ITEM_HEIGHT = 48;
@@ -154,18 +160,8 @@ export default function Form({ formData, setFormData }: IFormProps): JSX.Element
           </Grid>
           <Grid item xs={12}>
             {/* WYBÓR SYSTEMU */}
-        <ListItem>
-              <Stack spacing={2}>
-                <Typography variant="h4">Wybór systemu:</Typography>
-                <Box>
-                  <Grid container spacing={3}>
-                    <SystemIcon url={urlASRS} alt="ASRS" formData={formData} setFormData={setFormData} />
-                    <SystemIcon url={urlLRKPRK} alt="LRK&PRK" formData={formData} setFormData={setFormData} />
-                    <SystemIcon url={urlAGV} alt="AGV" formData={formData} setFormData={setFormData} />
-                    <SystemIcon url={urlAutoVNA} alt="AutoVNA" formData={formData} setFormData={setFormData} />
-                  </Grid>
-                </Box>
-              </Stack>
+            <ListItem>
+              <SystemSelector formData={formData} systems={systems} setFormData={setFormData} />
             </ListItem>
           </Grid>
         </Grid>
---
+++ src/components/SystemSelector.tsx
@@ -0,0 +1,106 @@
+import { Box, Checkbox, Grid, Stack, Typography } from "@mui/material";
+import { ISystems } from "./Form";
+import { IFormData } from "../App";
+
+export default function SystemSelector({ systems, formData, setFormData }: { systems: ISystems; formData: IFormData; setFormData: React.Dispatch<React.SetStateAction<IFormData>> }) {
+    function SystemIcon({ url, alt, formData, setFormData }: { url: string; alt: string; formData: IFormData; setFormData: React.Dispatch<React.SetStateAction<IFormData>> }) {
+        const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+      
+        const handleSystemChange = (alt: string) => {
+          setFormData((prevFormData) => ({
+            ...prevFormData,
+            system: {
+              ...prevFormData.system,
+              [alt.toLowerCase()]: !prevFormData.system[alt.toLowerCase()],
+            },
+          }));
+        };
+      
+        return (
+          <Grid item xs={6} sm={3}>
+            <Checkbox
+              sx={{ p: 0 }}
+              {...label}
+              checked={formData.system[alt.toLowerCase()]}
+              onChange={e => handleSystemChange(alt)}
+              icon={
+                <Box className="system-icon" style={{ width: '100%' }}>
+                  <img
+                    style={{
+                      filter: 'saturate(0%)',
+                      width: '100%',
+                      aspectRatio: '1/1',
+                      objectFit: 'cover',
+                      borderRadius: '.25rem',
+                      position: 'relative',
+                    }}
+                    src={url}
+                    alt={alt}
+                  />
+                  <Typography
+                    className="overlay-text"
+                    variant="h5"
+                    style={{
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      color: '#fff',
+                      textShadow: '0 0 .4rem black',
+                      textAlign: 'center',
+                    }}
+                  >
+                    {alt}
+                  </Typography>
+                </Box>
+              }
+              checkedIcon={
+                <Box className="system-icon" style={{ width: '100%' }}>
+                  <img
+                    style={{
+                      width: '100%',
+                      aspectRatio: '1/1',
+                      objectFit: 'cover',
+                      borderRadius: '.25rem',
+                      position: 'relative',
+                    }}
+                    src={url}
+                    alt={alt}
+                  />
+                  <Typography
+                    className="overlay-text"
+                    variant="h5"
+                    style={{
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%, -50%)',
+                      color: '#fff',
+                      textShadow: '0 0 .4rem black',
+                      textAlign: 'center',
+                    }}
+                  >
+                    {alt}
+                  </Typography>
+                </Box>
+              }
+            />
+          </Grid>
+        );
+    }
+    
+    return (
+        <Stack spacing={2}>
+            <Typography variant="h4">Wybór systemu:</Typography>
+            <Box>
+                <Grid container spacing={3}>
+                    {
+                        Object.entries(systems).map(([key, value]) => (
+                            <SystemIcon url={value.url} alt={value.alt} formData={formData} setFormData={setFormData} />
+                    ))
+                    }
+                </Grid>
+            </Box>
+        </Stack>
+    )
+}
