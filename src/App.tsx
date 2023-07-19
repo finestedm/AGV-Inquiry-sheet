50712d7cd8f9f9473041e4f43462553e5a902a6d
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2023-07-19 21:43:02 +0000

    work begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import Form from './components/Form';
 import TopBar from './components/AppBar';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Configure i18next
+i18n
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+    fallbackLng: 'en', // Fallback to English if the user's language is not supported
+    debug: true, // Enable debug mode for development
+    interpolation: {
+      escapeValue: false, // React already escapes string, so no need to escape again
+    },
+  });
 
 interface ISales {
   salesUnit: string;
@@ -72,10 +90,12 @@ function App() {
   const [formData, setFormData] = useInitialFormData();
 
   return (
+    <I18nextProvider i18n={i18n}>
     <div className="App">
       <TopBar formData={formData} setFormData={setFormData}/>
       <Form formData={formData} setFormData={setFormData} />
     </div>
+    </I18nextProvider>
   );
 }
 
