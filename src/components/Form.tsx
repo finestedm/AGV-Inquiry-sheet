45649e6b32a54ca6ef92a3461d0df4655b1f0d83
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2023-08-21 11:19:03 +0000

    different structure of steps

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -21,66 +21,79 @@ export default function Form(): JSX.Element {
 
   const formData = useSelector((state: RootState) => state.formData);
 
-  const [stepsCombined, setStepsCombined] = useState<{ [key: string]: { label: string, component: React.ReactNode } }>({
-    sales: {
-      label: t('steps.sales'), component: <FormSalesUnitStep key="sales" />
+  const [stepsCombined, setStepsCombined] = useState<{ label: string, component: React.ReactNode }[]>([
+    {
+      label: t('steps.sales'),
+      component: <FormSalesUnitStep key="sales" />
     },
-    customer: {
-      label: t('steps.customer'), component: <FormCustomerStep key="customer" />
+    {
+      label: t('steps.customer'),
+      component: <FormCustomerStep key="customer" />
     },
-    project: {
-      label: t('steps.project'), component: <FormProjectStep key="project" />
+    {
+      label: t('steps.project'),
+      component: <FormProjectStep key="project" />
     },
-    system: {
-      label: t('steps.system'), component: <FormSystemSelectorStep key="system" />
-    },
-  })
+    {
+      label: t('steps.system'),
+      component: <FormSystemSelectorStep key="system" />
+    }
+  ]);
 
   useEffect(() => {
-    const newSteps = { ...stepsCombined };
-
+    const newSteps: { label: string, component: React.ReactNode }[] = [
+      {
+        label: t('steps.sales'),
+        component: <FormSalesUnitStep key="sales" />
+      },
+      {
+        label: t('steps.customer'),
+        component: <FormCustomerStep key="customer" />
+      },
+      {
+        label: t('steps.project'),
+        component: <FormProjectStep key="project" />
+      },
+      {
+        label: t('steps.system'),
+        component: <FormSystemSelectorStep key="system" />
+      }
+    ];
+  
     if (formData.system.asrs.selected) {
-      newSteps.asrs = {
+      newSteps.push({
         label: t("steps.systems.asrs"),
         component: <FormASRSStep key="asrs" />,
-      };
-    } else {
-      delete newSteps.asrs; // Remove the key if system is deselected
+      });
     }
-
+  
     if (formData.system.lrkprk.selected) {
-      newSteps.lrkprk = {
+      newSteps.push({
         label: t("steps.systems.lrkprk"),
         component: <FormASRSStep key="lrkprk" />,
-      };
-    } else {
-      delete newSteps.lrkprk; // Remove the key if system is deselected
+      });
     }
-
+  
     if (formData.system.agv.selected) {
-      newSteps.agv = {
+      newSteps.push({
         label: t("steps.systems.agv"),
         component: <FormASRSStep key="agv" />,
-      };
-    } else {
-      delete newSteps.agv; // Remove the key if system is deselected
+      });
     }
-
+  
     if (formData.system.autovna.selected) {
-      newSteps.autovna = {
+      newSteps.push({
         label: t("steps.systems.autovna"),
         component: <FormASRSStep key="autovna" />,
-      };
-    } else {
-      delete newSteps.autovna; // Remove the key if system is deselected
+      });
     }
-
+  
     setStepsCombined(newSteps);
   }, [formData]);
-
+  
 
   const [activeStep, setActiveStep] = useState<number>(0);
-  const stepLabels = Object.values(stepsCombined).map((step) => step.label);
+  const stepLabels = stepsCombined.map((step) => step.label);
 
   const [fadeOut, setFadeOut] = useState<boolean>(false);
   const handleNext = () => {
@@ -108,7 +121,7 @@ export default function Form(): JSX.Element {
   };
 
 
-  const steps = Object.values(stepsCombined).map((step) => step.component);
+  const steps = stepsCombined.map((step) => step.component);
 
   if (formData) {
     return (
