92236661320f3b1f600981737f10d15bed1e5213
Paweł <finestedm@gmail.com>
2023-09-03 20:28:05 +0200

    check for darkMode system preference

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,25 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Switch from '@mui/material/Switch';
-import { toggleDarkMode } from '../features/redux/reducers/darkModeSlice';
+import { setDarkMode } from '../features/redux/reducers/darkModeSlice';
 import { RootState } from '../features/redux/store';
-import { Box, Button, Collapse, Fade, Stack, ToggleButton, Typography } from '@mui/material';
+import { Button, Collapse, Typography } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
-import { useEffect } from 'react';
 
 export default function DarkModeSwitch({ fullWidth }: { fullWidth: boolean }): JSX.Element {
   const dispatch = useDispatch();
   const darkMode = useSelector((state: RootState) => state.darkMode);
 
   useEffect(() => {
-    console.log(darkMode)
-  }, [darkMode])
+    // Check if the system prefers dark mode
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    prefersDarkMode ? dispatch(setDarkMode(true)) : dispatch(setDarkMode(false))
+  }, []);
 
   return (
     <Button
       fullWidth={fullWidth}
       variant='outlined'
-      onClick={() => dispatch(toggleDarkMode())}
+      onClick={() => dispatch(setDarkMode(!darkMode))}
       startIcon={
         <Collapse sx={{ height: '1.6rem' }} orientation="horizontal" collapsedSize={0}
           in={darkMode}
