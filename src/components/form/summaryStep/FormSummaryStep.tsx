b6d80e585a1960473eeb907d4a402ff7629abbee
Pawel <finestedm@gmail.com>
2023-12-24 00:03:18 +0100

    progress in summary step

--- src/components/form/summaryStep/FormSummaryStep.tsx
+++ src/components/form/summaryStep/FormSummaryStep.tsx
@@ -1,4 +1,4 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, Stack, Typography, useTheme } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, InputLabel, Link, Stack, Typography, useTheme } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useSelector } from "react-redux";
@@ -6,17 +6,50 @@ import { RootState } from "../../../features/redux/store";
 import SystemAccordion from "./subcomponents/SystemAccordion";
 
 export default function FormSummaryStep() {
+    const formData = useSelector((state: RootState) => state.formData)
     const { t } = useTranslation();
     const theme = useTheme();
 
-    const selectedSystem = useSelector((state: RootState) => (
+    const selectedSystems = useSelector((state: RootState) => (
         Object.entries(state.formData.system).filter(([systemName, systemData]) => systemData.selected)
     ));
 
     return (
-        <Stack spacing={8}>
-            <Typography variant="h4" textAlign='left'>{t('summary.header')}</Typography>
-            <Stack spacing={2}>{selectedSystem.map(system => <SystemAccordion />)}</Stack>
+        <Stack spacing={8} textAlign='left'>
+            <Typography variant="h4">{t('summary.header')}</Typography>
+            <Stack spacing={2} className='summary-sales'>
+                <Typography variant="h5">{t('sales.header')}</Typography>
+                <Stack spacing={0}>
+                    <Typography>
+                        <Typography fontWeight={700}>
+                            {formData.sales.salesUnit}
+                        </Typography>
+                        {formData.sales.contactPerson}, {formData.sales.contactPersonRole}
+                    </Typography>
+                </Stack>
+            </Stack>
+            <Stack spacing={2} className='summary-customer'>
+                <Typography variant="h5">{t('customer.header')}</Typography>
+                <Stack spacing={0}>
+                    <Typography>
+                        <Typography fontWeight={700}>
+                            {formData.customer.name} <Typography color='text.secondary' component="span">({formData.customer.sapNumber})</Typography>
+                        </Typography>
+                        {formData.customer.address}
+                    </Typography>
+                </Stack>
+                <Stack spacing={0}>
+                    <Typography>
+                        <Typography fontWeight={700}>
+                            {formData.customer.contactPerson} <Typography color='text.secondary' component="span">, {formData.customer.contactPersonRole}</Typography>
+                        </Typography>
+                        <Link href={`tel:${formData.customer.contactPersonPhone}`}>{formData.customer.contactPersonPhone}</Link>
+                        , <Link href={`mailto:${formData.customer.contactPersonMail}`}>{formData.customer.contactPersonMail}</Link>
+
+                    </Typography>
+                </Stack>
+            </Stack>
+            <Stack spacing={4}>{selectedSystems.map(system => <SystemAccordion />)}</Stack>
         </Stack>
     )
 }
