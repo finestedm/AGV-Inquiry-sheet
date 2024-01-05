8fa35f863778cb8c8114a57a4ebf9dd975d02d33
Paweł Stępień <85036437+finestedm@users.noreply.github.com>
2024-01-05 15:03:16 +0000

    splitting gannt

--- src/components/form/projectStep/subcomponents/GanttGraph.tsx
+++ src/components/form/projectStep/subcomponents/GanttGraph.tsx
@@ -10,12 +10,8 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import EditIcon from '@mui/icons-material/Edit';
-import CloseIcon from '@mui/icons-material/Close';
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { DateCalendar, DatePicker, LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
 import { ExtendedTask, IMilestones } from "../../../../features/interfaces";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs, { Dayjs } from "dayjs";
 import 'dayjs/locale/pl';
 import DateEditDialog from "./DateEditDialog";
 
@@ -31,6 +27,9 @@ export default function GanttGraph(): JSX.Element {
     const [viewMode, setViewMode] = useState<string>('Month')
     const [selectedTask, setSelectedTask] = useState<ExtendedTask | null>(null);
     const [dateEditDialogOpen, setDateEditDialogOpen] = useState(false);
+    const taskListHeight = 50;
+    const headerHeight = 60;
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
     const uneditableTasks: (keyof IMilestones)[] = ['launch' as keyof IMilestones]
 
@@ -56,6 +55,7 @@ export default function GanttGraph(): JSX.Element {
             const end = new Date(date.end);
             return {
                 id: name,
+                rowHeight: taskListHeight - 3,
                 name: t(`project.milestones.${name}`),
                 start,
                 end,
@@ -63,90 +63,99 @@ export default function GanttGraph(): JSX.Element {
                 progress: 0,
                 isDisabled: !editMode || isTaskUneditable(name as keyof IMilestones),
                 styles: { backgroundColor: (name === 'order' || name === 'launch') ? theme.palette.secondary.main : theme.palette.primary.main },
-
             };
         });
     })();
 
-    function CustomListTable({ tasks, setSelectedTask, selectedTaskId, rowHeight, }: { tasks: Task[]; setSelectedTask: (taskId: string) => void; selectedTaskId: string; rowHeight: number }) {
+    function CustomListTable() {
 
         const theme = useTheme();
         return (
-            <TableContainer component={Box} sx={{ border: 1, borderRight: 0, borderRadius: `${theme.shape.borderRadius}px 0 0 ${theme.shape.borderRadius}px`, borderColor: theme.palette.divider, minWidth: '350px' }} >
-                <Table>
-                    <TableHead sx={{ height: rowHeight }}>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Date Range</TableCell>
-                            {editMode && <TableCell>Edit</TableCell>}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {tasks.map((task) => (
-                            <TableRow
-                                key={task.id}
-                                onClick={() => setSelectedTask(task.id)}
-                                selected={task.id === selectedTaskId}
-                                style={{ height: rowHeight }}
-                            >
-                                <TableCell><Typography fontSize='85%'>{task.name}</Typography></TableCell>
-                                <TableCell>
-                                    <Typography fontSize='85%'>
-                                        {
-                                            task.type === 'milestone' ?
-                                                task.start.toLocaleDateString()
-                                                :
-                                                `${task.start.toLocaleDateString()} - ${task.end.toLocaleDateString()}`
-                                        }
-                                    </Typography>
-                                </TableCell>
-                                {editMode &&
-                                    <TableCell>
-                                        <IconButton
-                                            size='small'
-                                            disabled={isTaskUneditable(task.id as keyof IMilestones)}
-                                            onClick={() => handledateEditDialogOpen(task as ExtendedTask)}
-                                        >
-                                            <EditIcon />
-                                        </IconButton>
-                                    </TableCell>
-                                }
+            <Paper elevation={0} sx={{ borderRadius: theme.shape.borderRadius, border: 1, borderColor: theme.palette.divider }}>
+                <TableContainer component={Box} >
+                    <Table>
+                        <TableHead sx={{ height: headerHeight }}>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Date Range</TableCell>
+                                {editMode && <TableCell>Edit</TableCell>}
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {milestones.map((task) => (
+                                <TableRow
+                                    key={task.id}
+                                    style={{ height: taskListHeight }}
+                                >
+                                    <TableCell><Typography fontSize='85%'>{task.name}</Typography></TableCell>
+                                    <TableCell>
+                                        <Typography fontSize='85%'>
+                                            {
+                                                task.type === 'milestone' ?
+                                                    task.start.toLocaleDateString()
+                                                    :
+                                                    `${task.start.toLocaleDateString()} - ${task.end.toLocaleDateString()}`
+                                            }
+                                        </Typography>
+                                    </TableCell>
+                                    {editMode &&
+                                        <TableCell>
+                                            <IconButton
+                                                size='small'
+                                                disabled={isTaskUneditable(task.id as keyof IMilestones)}
+                                                onClick={() => handledateEditDialogOpen(task as ExtendedTask)}
+                                            >
+                                                <EditIcon />
+                                            </IconButton>
+                                        </TableCell>
+                                    }
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+
         );
     }
 
-
-
     return (
-        <Box position='relative' className={theme.palette.mode === 'dark' ? 'ganttchart-container-dark' : 'ganttchart-container'} sx={{ borderColor: theme.palette.divider }}>
-            <Gantt
-                tasks={milestones}
-                barCornerRadius={theme.shape.borderRadius}
-                barBackgroundSelectedColor={theme.palette.secondary.main}
-                arrowIndent={40}
-                todayColor={theme.palette.divider}
-                viewMode={viewMode as ViewMode}
-                // preStepsCount={0}
-                locale='pl'
-                fontSize=".75rem"
-                listCellWidth={viewTaskList ? '100px' : ""}
-                columnWidth={columnsWidth}
-                TooltipContent={CustomTooltip}
-                TaskListHeader={() => null}
-                TaskListTable={CustomListTable}
-                onDateChange={(task: Task) => {
-                    const { id, start, end } = task;
-                    dispatch(handleDateChanges({ id, start, end }));
-                }}
-                onDoubleClick={(task: Task) => !isTaskUneditable(task.id as keyof IMilestones) && handledateEditDialogOpen(task as ExtendedTask)}
-            />
-            <Box position='absolute' top='10%' right={25}>
-                <SizeEditButtons columnsWidth={columnsWidth} setColumnWidth={setColumnWidth} viewTaskList={viewTaskList} setViewTaskList={setViewTaskList} viewMode={viewMode} setViewMode={setViewMode} />
-            </Box>
+        <Box className={theme.palette.mode === 'dark' ? 'ganttchart-container-dark' : 'ganttchart-container'} >
+            <Grid container >
+                <Grid item xs={12} lg={4}><CustomListTable /></Grid>
+                <Grid item xs={12} lg={8} position='relative'>
+                    <Box border={1} sx={{ borderColor: theme.palette.divider }}>
+                        <Gantt
+                            tasks={milestones}
+                            barCornerRadius={theme.shape.borderRadius}
+                            barBackgroundSelectedColor={theme.palette.secondary.main}
+                            arrowIndent={40}
+                            todayColor={theme.palette.divider}
+                            viewMode={viewMode as ViewMode}
+                            // preStepsCount={0}
+                            locale='pl'
+                            fontSize=".75rem"
+                            listCellWidth={viewTaskList ? '100px' : ""}
+                            columnWidth={columnsWidth}
+                            TooltipContent={CustomTooltip}
+                            TaskListHeader={() => null}
+                            headerHeight={headerHeight}
+                            TaskListTable={() => null}
+                            onDateChange={(task: Task) => {
+                                const { id, start, end } = task;
+                                dispatch(handleDateChanges({ id, start, end }));
+                            }}
+                            onDoubleClick={(task: Task) => !isTaskUneditable(task.id as keyof IMilestones) && handledateEditDialogOpen(task as ExtendedTask)}
+                        />
+                    </Box>
+                    <Box position='absolute' top='10%' right={25}>
+                        <SizeEditButtons columnsWidth={columnsWidth} setColumnWidth={setColumnWidth} viewTaskList={viewTaskList} setViewTaskList={setViewTaskList} viewMode={viewMode} setViewMode={setViewMode} />
+                    </Box>
+                    <Box position='absolute' top='50%' left={-5}>
+                        <Button variant='contained' onClick={() => console.log('eee')}>eeeee </Button>
+                    </Box>
+                </Grid>
+            </Grid>
             {selectedTask && <DateEditDialog selectedTask={selectedTask} dateEditDialogOpen={dateEditDialogOpen} handleDialogClose={handleDialogClose} />}
         </Box>
     )
